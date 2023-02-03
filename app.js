9bb00760bc569899f3a40f23b139e9f3bdb5c914
kalpeshEcosmob.com <kalpesh.makwana@ecosmob.com>
2023-02-03 19:16:47 +0530

    03/02/2023

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql2 = require("mysql2");
+
+const pool = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Admin@123",
+  database: 'Resume'
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,8 +19,24 @@ const PORT = 3000;
 
 app.get("/getData", async (req, res, next) => {
   try {
-    res.json("Working")
-  } catch (error) {}
+
+    let aboutMe;
+    aboutMe =  pool.query("select * from tbl_cv", function (err, results) {
+      
+      // results = results.map(row => (row.package = JSON.parse(row.package), row));
+
+      aboutMe = JSON.parse(results[0]['about_me']);
+
+      console.log('aboutMe ------------', aboutMe)
+    });
+
+    console.log('aboutMe', aboutMe)
+
+    
+    
+  } catch (error) {
+    console.log('error', error)
+  }
 });
 
 app.post("/postData", async (req, res, next) => {
