5ac466c892532d67b2a69613655615bce3304835
kalpeshEcosmob.com <kalpesh.makwana@ecosmob.com>
2023-02-06 11:10:14 +0530

    new commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pool = mysql2.createConnection({
   host: "localhost",
   user: "root",
   password: "Admin@123",
-  database: 'Resume'
+  database: 'Resume_'
 });
 
 const app = express();
@@ -19,20 +19,19 @@ const PORT = 3000;
 
 app.get("/getData", async (req, res, next) => {
   try {
-
-    let aboutMe;
-    aboutMe =  pool.query("select * from tbl_cv", function (err, results) {
+    const query = "select * from tbl_cv";
+    const aboutMe =  pool.query(query, function (err, results) {
       
       // results = results.map(row => (row.package = JSON.parse(row.package), row));
 
-      aboutMe = JSON.parse(results[0]['about_me']);
+      // aboutMe = JSON.parse(results[0]['about_me']);
 
-      console.log('aboutMe ------------', aboutMe)
+      console.log('aboutMe -----------',results)
+      // console.log('aboutMe ------------', aboutMe)
+      res.json("Hello")
     });
 
     console.log('aboutMe', aboutMe)
-
-    
     
   } catch (error) {
     console.log('error', error)
@@ -41,10 +40,32 @@ app.get("/getData", async (req, res, next) => {
 
 app.post("/postData", async (req, res, next) => {
   try {
-  } catch (error) {}
+    const {emp_id,emp_email,image,resume_data} = req.body;
+    let insertData;
+    const query = "INSTERT INTO Resume_(emp_id,emp_email,image,resume_data) VALUES ?"
+    const value = [
+      [emp_id, emp_email,image,resume_data],
+    ];
+   insertData =  pool.query(query,[value], function (err, results) {
+      
+      // results = results.map(row => (row.package = JSON.parse(row.package), row));
+
+      // aboutMe = JSON.parse(results[0]['about_me']);
+
+      console.log('aboutMe -----------',results)
+      // console.log('aboutMe ------------', aboutMe)
+      res.json("Hello")
+    });
+
+    console.log('aboutMe', aboutMe)
+
+  } catch (error) {
+    console.log('error', error)
+  }
 });
 
 app.use((error, req, res, next) => {
+  console.log('errorr')
   res
     .status(500)
     .json({ Error: "Invalid Request", message: "Please check your url" });
---
+++ data.js
@@ -0,0 +1,136 @@
+const data = {
+  aboutMe: {
+    candidate_code: "001",
+    description:
+      "Innovative, task-driven professional with 8+ years of experience in \nweb design and development across diverse industries. Equipped \nwith a record of success in consistently identifying and providing the technological needs of companies through ingenious innovation. \nProficient in developing databases, creating user interfaces, writing \nand testing codes, troubleshooting simple/complex issues, and \nimplementing new features based on user feedback.\n",
+    education: [
+      {
+        education: "Bechelor of  computer application",
+      },
+      {
+        education: "Master of science in Information and Technology",
+      },
+      {
+        education: "PHD",
+      },
+    ],
+    role: "Senior Softwear engineer",
+    experienceYear: 4,
+    experienceMonth: 3,
+  },
+  skills: [
+    {
+      skill: "react",
+      Rating: 7,
+    },
+    {
+      skill: "node",
+      Rating: 7,
+    },
+    {
+      skill: "angular",
+      Rating: 8,
+    },
+    {
+      skill: "mongoDB",
+      Rating: 9,
+    },
+  ],
+  experience: [
+    {
+      title: "project1",
+      technologies: "react, node",
+      about_project:
+        "Softswitch is an application that enables to create various billing rate plans, create distributors, resellers or groups, and subscribers. Service providers also configure media gateways-related information, manage media-related gateway information, and manage various value-added services\n",
+      description: [
+        {
+          first:
+            "Gathering business requirements, and analyzing the business. Writing test cases according to functional specifications. \n",
+        },
+        {
+          first: "Understanding the test plan. \n",
+        },
+        {
+          first:
+            "Performed functional & system testing by performing load tests to check the application's stability. \n",
+        },
+      ],
+      role: "Senior software engineer",
+    },
+    {
+      title: "project2",
+      technologies: "monogDB, angular",
+      about_project:
+        "Softswitch is an application that enables to create various billing rate plans, create distributors, resellers or groups, and subscribers. Service providers also configure media gateways-related information, manage media-related gateway information, and manage various value-added services",
+      description: [
+        {
+          first:
+            "Executing test cases to validate the functionality and transactions by checking the Log files.\n",
+        },
+        {
+          first:
+            "logging defects in the defect log sheet and assigning the defects log sheet to the application team. \n",
+        },
+        {
+          first: "Responsible for reporting the bugs to the application team. ",
+        },
+        {
+          first:
+            "Tested the VOIP components like SIPProxy, MediaProxy, and IVR extensively. \n",
+        },
+      ],
+      role: "Jr. Software developer",
+    },
+  ],
+  extraSkill: {
+    certificate: [
+      {
+        certificate: "certi1",
+      },
+      {
+        certificate: "certi2",
+      },
+      {
+        certificate: "certi3",
+      },
+    ],
+    award: [
+      {
+        award: "award1",
+      },
+      {
+        award: "award2",
+      },
+    ],
+    training: [
+      {
+        training: "training1",
+      },
+      {
+        training: "training2",
+      },
+    ],
+  },
+};
+
+
+// console.log('===========>',data)
+// console.log(JSON.stringify(data))
+
+const check = JSON.stringify(data);
+
+// console.log('typeof',typeof(check))
+
+console.log("===============>",JSON.parse(check));
+
+
+console.log(typeof(data))
+
+
+
+
+
+"INSTERT INTO Resume_(emp_id,emp_email,image,resume_data) VALUES ?";
+const value = [
+  [emp_name, emp_destination, emp_gender, emp_salary, studentid],
+];
